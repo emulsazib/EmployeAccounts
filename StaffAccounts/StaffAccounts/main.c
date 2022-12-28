d123f55c95d92fee80ef3d5a5b2988d933419af7
Emul Ahamed Sazib <110263665+emulsazib@users.noreply.github.com>
2022-12-29 02:34:16 +0600

    Update main.c

--- StaffAccounts/StaffAccounts/main.c
+++ StaffAccounts/StaffAccounts/main.c
@@ -1,14 +1,14 @@
 //
 //  main.c
-//  StaffAccounts
+//  EmplyeAccounts
 //
 //  Created by Sajib mac mini on 29/12/22.
 //
 
 #include <stdio.h>
 #include <stdlib.h>
-struct Staff{
-        int staffnumber;
+struct Employe{
+        int employenumber;
         char name[20];
         int attendence;
         double overTime;
@@ -21,7 +21,7 @@ struct Staff{
         double extra;
     };
 int main(int argc, const char * argv[]) {
-    struct Staff staff[2];
+    struct Employe employe[2];
         int select,menuNum,secondmenuNam,givenPin;
         int count=1;
         int pin=1234;
@@ -37,10 +37,10 @@ int main(int argc, const char * argv[]) {
         }
         //Manu
        while(count!=0){
-        printf("\n1. Add Staff");
-        printf("\n2. Display Staffs");
-        printf("\n3. Account's Staffs");
-        printf("\n4. Staffs info");
+        printf("\n1. Add employe");
+        printf("\n2. Display employe");
+        printf("\n3. Account's employe");
+        printf("\n4. Employe info");
         printf("\n5. Exit");
         //Choice the menu
         printf("\nEnter the menu number: ");
@@ -48,19 +48,19 @@ int main(int argc, const char * argv[]) {
         switch(menuNum){
     case 1:
         for (int i =0;i<2;i++){
-                printf("\nEnter the Number of Staff: ");
-                scanf("%d",&staff[i].staffnumber);
-                    if(staff[i].staffnumber!=0){
-                printf("\nEnter the Name of Staff: ");
-                scanf("%s",&staff[i].name);
-                printf("\nEnter the Salary of Staff: ");
-                scanf("%lf",&staff[i].salary);
-                printf("\nEnter the Attendence of Staff: ");
-                scanf("%d",&staff[i].attendence);
-                printf("\nEnter the Over time of Staff: ");
-                scanf("%lf",&staff[i].overTime);
-                printf("\nEnter the Taken Amount of Staff: ");
-                scanf("%lf",&staff[i].takenAmount);
+                printf("\nEnter the Number of employe: ");
+                scanf("%d",&employe[i].employenumber);
+                    if(employe[i].employenumber!=0){
+                printf("\nEnter the Name of employe: ");
+                scanf("%s",&employe[i].name);
+                printf("\nEnter the Salary of employe: ");
+                scanf("%lf",&employe[i].salary);
+                printf("\nEnter the Attendence of employe: ");
+                scanf("%d",&employe[i].attendence);
+                printf("\nEnter the Over time of employe: ");
+                scanf("%lf",&employe[i].overTime);
+                printf("\nEnter the Taken Amount of employe: ");
+                scanf("%lf",&employe[i].takenAmount);
                 printf("\n============================\n");
 
                     }else{
@@ -68,29 +68,29 @@ int main(int argc, const char * argv[]) {
                     }
         }
         for(int i=0;i<2;i++){
-            printf("%d",staff[i].staffnumber);
-            printf(" %s\n",staff[i].name);
+            printf("%d",employe[i].employenumber);
+            printf(" %s\n",employe[i].name);
             printf("\n============================\n");
         }
         break;
     case 2:
         printf("\n============================\n");
         for(int i=0;i<2;i++){
-            printf("%d",staff[i].staffnumber);
-            printf(" %s\n",staff[i].name);
+            printf("%d",employe[i].employenumber);
+            printf(" %s\n",employe[i].name);
         }
         printf("\n============================\n");
         break;
     case 3:
         printf("\n============================\n");
         for(int i=0;i<2;i++){
-            printf("%d",staff[i].staffnumber);
-            printf(" %s\n",staff[i].name);
+            printf("%d",employe[i].employenumber);
+            printf(" %s\n",employe[i].name);
         }
         printf("\n============================\n");
-        printf("\nSelect the Staff: ");
+        printf("\nSelect the Employe: ");
         scanf("%d",&select);
-        //staff[select];
+        //employe[select];
         //Acounts menu...
         printf("\n1. Attendence Salary");
         printf("\n2. Over Time Salary");
@@ -102,34 +102,34 @@ int main(int argc, const char * argv[]) {
         switch(secondmenuNam){
     case 1:
         printf("\n============================\n");
-        staff[select-1].attendenceSalary=(staff[select-1].salary/30)*staff[select-1].attendence;
-        printf("%s Attendence Salary: %.2lf",staff[select-1].name,staff[select-1].attendenceSalary);
+        employe[select-1].attendenceSalary=(employe[select-1].salary/30)*employe[select-1].attendence;
+        printf("%s Attendence Salary: %.2lf",employe[select-1].name,employe[select-1].attendenceSalary);
         printf("\n============================\n");
         break;
     case 2:
         printf("\n============================\n");
-        staff[select-1].overTimeSalary=((staff[select-1].salary/30)/8)*staff[select-1].overTime;
-        printf("%s Over Time Salary: %.2lf",staff[select-1].name,staff[select-1].overTimeSalary);
+        employe[select-1].overTimeSalary=((employe[select-1].salary/30)/8)*employe[select-1].overTime;
+        printf("%s Over Time Salary: %.2lf",employe[select-1].name,employe[select-1].overTimeSalary);
         printf("\n============================\n");
         break;
     case 3:
         printf("\n============================\n");
-        staff[select-1].totalSalary=staff[select-1].attendenceSalary+staff[select-1].overTimeSalary;
-        printf("%s Total Salary: %.2lf",staff[select-1].name,staff[select-1].totalSalary);
+        employe[select-1].totalSalary=employe[select-1].attendenceSalary+employe[select-1].overTimeSalary;
+        printf("%s Total Salary: %.2lf",employe[select-1].name,employe[select-1].totalSalary);
         printf("\n============================\n");
         break;
     case 4:
         printf("\n============================\n");
-        if(staff[select-1].totalSalary>staff[select-1].takenAmount)
-        staff[select-1].due=staff[select-1].totalSalary-staff[select-1].takenAmount;
-        printf("%s Due Salary: %.2lf",staff[select-1].name,staff[select-1].due);
+        if(employe[select-1].totalSalary>employe[select-1].takenAmount)
+        employe[select-1].due=employe[select-1].totalSalary-employe[select-1].takenAmount;
+        printf("%s Due Salary: %.2lf",employe[select-1].name,employe[select-1].due);
         printf("\n============================\n");
         break;
     case 5:
         printf("\n============================\n");
-        if(staff[select-1].totalSalary<staff[select-1].takenAmount)
-        staff[select-1].extra=staff[select-1].takenAmount-staff[select-1].totalSalary;
-        printf("%s Due Salary: %.2lf",staff[select-1].name,staff[select-1].extra);
+        if(employe[select-1].totalSalary<employe[select-1].takenAmount)
+        employe[select-1].extra=employe[select-1].takenAmount-employe[select-1].totalSalary;
+        printf("%s Due Salary: %.2lf",employe[select-1].name,employe[select-1].extra);
         printf("\n============================\n");
         break;
         }
